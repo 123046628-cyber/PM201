d8107f79d342e32edcdc34b065cdaddccc78f3c6
123046628-cyber <123046628@upq.edu.mx>
2026-06-08 22:43:38 -0600

    Practica6ConTarea

--- Practica5/myApp2/App.js
+++ Practica5/myApp2/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import {Saludo} from './components/Saludo'; /* Importación de un componente propio */
 import {Saludo2} from './components/Saludo2';
+import {Perfil} from './components/Perfil';
 
 /* Zona 2: Main - Componentes */
 export default function App() {
@@ -15,6 +16,8 @@ export default function App() {
       <Saludo/> {/* Componente propio */}
       <Text> ------------------- </Text>
       <Saludo2/> {/* Componente propio con tres elementos */}
+      <Text> ------------------- </Text>
+      <Perfil/> {/* Componente propio con cuatro elementos */}
 
       <StatusBar style="auto" />
 
---
+++ Practica5/myApp2/components/Perfil.js
@@ -0,0 +1,16 @@
+/* Componente de 4 elementos */
+import { View, Text} from 'react-native';
+
+export const Perfil = () => { 
+    return (
+        <View>
+
+            <Text> Nombre: Huerta Frias Danna Zoe </Text>
+            <Text> Carrera: Ingeniería en Sistemas Computacionales </Text>
+            <Text> Materia: Programación Móvil </Text>
+            <Text> Cuatrimestre: 9no </Text>
+
+        </View>
+        
+    )
+}
